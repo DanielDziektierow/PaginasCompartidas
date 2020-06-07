d8b1d9e8de2329b32be64d927021a403df990da0
DanielDziektierow <dz09dz@hotmail.com>
2020-06-07 20:25:32 -0300

    Empezando..

---
+++ Liquidacion/intro.js
@@ -0,0 +1,11 @@
+/*alert("LIQUIDACIONES");
+var NombreProyecto;
+NombreProyecto=prompt("Ingresa nombre del destino de la liquidacion");
+document.write(NombreProyecto);
+*/
+var suma = function(encargado, ayudante){
+    var encargado= parseFloat(document.getElementById("encargado").value);
+    var ayudante= parseFloat(document.getElementById("ayudante").value);
+    return saldoAnterior + precio;
+}
+document.write(suma(10, 50))
